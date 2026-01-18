153f4965485aa3de9032b7e2f524d5d358f5c9b2
AAATomik <tomhaj1982@gmail.com>
2026-01-18 17:53:41 +0100

    Simplify Railway config: use npm start with 0.0.0.0 hostname

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Ensure Next.js listens on all interfaces
+  server: {
+    hostname: '0.0.0.0',
+  },
 }
 
 module.exports = nextConfig
