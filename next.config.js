4e40911c4c3ebb6dabb4c7ecea0e0cf2a69f5b84
AAATomik <tomhaj1982@gmail.com>
2026-01-18 18:01:25 +0100

    Remove server hostname configuration from Next.js setup

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Ensure Next.js listens on all interfaces
-  server: {
-    hostname: '0.0.0.0',
-  },
 }
 
 module.exports = nextConfig
